cd146dd5853c042f28f2798c47a4e308d768c47f
MdSagorBepary~ <acchub490@gmail.com>
2022-09-23 21:18:42 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// function plusFunction(anyId){
+// let takingValue=document.getElementById(anyId).value;
+// let takingValIntoNum=parseFloat(takingValue);
+// console.log(takingValIntoNum);
+// document.getElementById(anyId).value= takingValIntoNum+1;
+// }
+// const  mytarget= document.getElementById("maxBtn");
+// mytarget.addEventListener("click",function(){
+//     console.log("you clicked me");
+// })
